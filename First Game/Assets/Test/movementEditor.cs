9784eaff4759974cbe3801776f77709a26b57164
Manakir <zimmerzalor@gmail.com>
2021-05-29 10:50:30 +0300

    update

---
+++ First Game/Assets/Test/movementEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movementEditor : MonoBehaviour
+{
+    [SerializeField, Range(0f, 100f)]
+    float maxSpeed = 10f;
+    void Update()
+    {
+        Vector2 playerInput;
+        playerInput.x = Input.GetAxis("Horizontal");
+        playerInput.y = Input.GetAxis("Vertical");
+        Vector3 velocity = new Vector3(playerInput.x, playerInput.y, 0f) * maxSpeed;
+        Vector3 displacement = velocity * Time.deltaTime;
+        transform.localPosition += displacement;
+    }
+}
